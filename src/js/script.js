c58933a32e3f8c730568368039319cc5931078df
yutaro <yutarotweet@gmail.com>
2023-08-03 19:31:19 +0900

    service終了

--- src/js/script.js
+++ src/js/script.js
@@ -20,3 +20,28 @@ $(".drawer__icon").on("click", function (e) {
   // セミコロン忘れない！
 
 });
+
+// swiper initialize
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+//
