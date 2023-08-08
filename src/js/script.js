9531fb62bd8473c4e926753b43ec77ae48363b81
yutaro <yutarotweet@gmail.com>
2023-08-08 20:14:45 +0900

    8/8時点

--- src/js/script.js
+++ src/js/script.js
@@ -25,23 +25,32 @@ $(".drawer__icon").on("click", function (e) {
 
 const swiper = new Swiper('.swiper', {
   // Optional parameters
-  direction: 'vertical',
+  direction: 'horizontal',
   loop: true,
-
+  slidesPerView: 1.25, // 一度に表示する枚数
+  breakpoints: {
+    // スライドの表示枚数：500px以上の場合
+    768: {
+      slidesPerView: 2.5,
+      spaceBetween: 40, // スライド間の距離
+    },
+  },
+  
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
   },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
 });
 //
+
+// qa-box
+$('.qa__box__q__wrapper').on("click", function() {
+  $(this).next().slideToggle();
+  /* qa__box__qクラスがクリックされたら
+  クリックされたクラスの、次にあたるクラスを、スライドさせて表示させるよという意味 */
+  $(this).find('.qa__box__icon').toggleClass('is-open');
+  
+  });
+  // 
