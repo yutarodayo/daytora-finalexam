e6b78310ccde1a917b5f09be4a468f5f1958283c
yutaro <yutarotweet@gmail.com>
2023-08-10 16:10:00 +0900

    ピクパ前

--- src/js/script.js
+++ src/js/script.js
@@ -54,3 +54,64 @@ $('.qa__box__q__wrapper').on("click", function() {
   
   });
   // 
+
+  // スムーススクロール
+  jQuery('a[href^="#"]').on("click", function () {
+    var header = jQuery(".header").innerHeight();
+    var id = jQuery(this).attr("href");
+    var position = 0;
+    if (id != "#") {
+      position = jQuery(id).offset().top - header;
+    }
+    jQuery("html,body").animate(
+      {
+        scrollTop: position,
+      },
+      200
+    );
+  });
+
+  // to_top
+  $(function(){
+    var topBtn=$('#page_top');
+    topBtn.hide();
+   
+    //ボタンの表示設定
+    $(window).scroll(function(){
+      if($(this).scrollTop()>500){
+        //---- 画面を500pxスクロールしたら、ボタンを表示する
+        topBtn.fadeIn();
+      }else{
+        //---- 画面が500pxより上なら、ボタンを表示しない
+        topBtn.fadeOut();
+      } 
+    });
+   
+    //ボタンをクリックしたら、おそらく200msでスクロールして上に戻る
+    topBtn.click(function(){
+      $('body,html').animate({
+      scrollTop: 0},200);
+      return false;
+    });
+  });
+
+// form 項目すべて入力でボタンON
+$(document).ready(function() {
+  const $submitBtn = $('#js-submit');
+
+  function checkFormInputs() {
+    if (
+      $('#form #select').val() !== "" &&
+      $('#form #kana').val() !== "" &&
+      $('#form input[type="text"]').val() !== "" &&
+      $('#form input[type="email"]').val() !== "" &&
+      $('#form input[type="checkbox"]').prop('checked') === true
+    ) {
+      $submitBtn.addClass('js-active').prop('disabled', false); // enabling button
+    } else {
+      $submitBtn.removeClass('js-active').prop('disabled', true); // disabling button
+    }
+  }
+
+  $('#form input,#form textarea,#form select').on('change input', checkFormInputs);
+});
